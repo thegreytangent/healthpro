3f7261270ee75c2fa1adae5aa33c796b9d10baa1
thegreytangent <thegreytangent@gmail.com>
2024-02-27 17:11:28 +0800

    Feat: Creating all controllers features

---
+++ app/Http/Controllers/BarangayController.php
@@ -0,0 +1,12 @@
+<?php
+
+    namespace App\Http\Controllers;
+
+    use Illuminate\Http\Request;
+
+    class BarangayController extends Controller
+    {
+        public function index() {
+            return 1;
+        }
+    }
