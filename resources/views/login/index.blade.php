74aafcbf4d9ffc019c7bb2bc55fc6390cd60af4d
thegreytangent <thegreytangent@gmail.com>
2024-02-27 16:51:00 +0800

    Feat: Edit navbar put logout button

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -1,1 +1,119 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>AdminLTE 3 | Log in</title>
+
+    <link rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+
+    <link rel="stylesheet" href="../../plugins/fontawesome-free/css/all.min.css">
+
+    <link rel="stylesheet" href="../../plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+
+    <link rel="stylesheet" href="../../dist/css/adminlte.min.css?v=3.2.0">
+    <script nonce="5fb63e1d-0fa4-4c1f-8d1b-3607249ae64e">try {
+            (function (w, d) {
+                !function (ct, cu, cv, cw) {
+                    ct[cv] = ct[cv] || {};
+                    ct[cv].executed = [];
+                    ct.zaraz = {deferred: [], listeners: []};
+                    ct.zaraz.q = [];
+                    ct.zaraz._f = function (cx) {
+                        return async function () {
+                            var cy = Array.prototype.slice.call(arguments);
+                            ct.zaraz.q.push({m: cx, a: cy})
+                        }
+                    };
+                    for (const cz of ["track", "set", "debug"]) ct.zaraz[cz] = ct.zaraz._f(cz);
+                    ct.zaraz.init = () => {
+                        var cA = cu.getElementsByTagName(cw)[0], cB = cu.createElement(cw),
+                            cC = cu.getElementsByTagName("title")[0];
+                        cC && (ct[cv].t = cu.getElementsByTagName("title")[0].text);
+                        ct[cv].x = Math.random();
+                        ct[cv].w = ct.screen.width;
+                        ct[cv].h = ct.screen.height;
+                        ct[cv].j = ct.innerHeight;
+                        ct[cv].e = ct.innerWidth;
+                        ct[cv].l = ct.location.href;
+                        ct[cv].r = cu.referrer;
+                        ct[cv].k = ct.screen.colorDepth;
+                        ct[cv].n = cu.characterSet;
+                        ct[cv].o = (new Date).getTimezoneOffset();
+                        if (ct.dataLayer) for (const cG of Object.entries(Object.entries(dataLayer).reduce(((cH, cI) => ({...cH[1], ...cI[1]})), {}))) zaraz.set(cG[0], cG[1], {scope: "page"});
+                        ct[cv].q = [];
+                        for (; ct.zaraz.q.length;) {
+                            const cJ = ct.zaraz.q.shift();
+                            ct[cv].q.push(cJ)
+                        }
+                        cB.defer = !0;
+                        for (const cK of [localStorage, sessionStorage]) Object.keys(cK || {}).filter((cM => cM.startsWith("_zaraz_"))).forEach((cL => {
+                            try {
+                                ct[cv]["z_" + cL.slice(7)] = JSON.parse(cK.getItem(cL))
+                            } catch {
+                                ct[cv]["z_" + cL.slice(7)] = cK.getItem(cL)
+                            }
+                        }));
+                        cB.referrerPolicy = "origin";
+                        cB.src = "/cdn-cgi/zaraz/s.js?z=" + btoa(encodeURIComponent(JSON.stringify(ct[cv])));
+                        cA.parentNode.insertBefore(cB, cA)
+                    };
+                    ["complete", "interactive"].includes(cu.readyState) ? zaraz.init() : ct.addEventListener("DOMContentLoaded", zaraz.init)
+                }(w, d, "zarazData", "script");
+            })(window, document)
+        } catch (e) {
+            throw fetch("/cdn-cgi/zaraz/t"), e;
+        }
+        ;</script>
+</head>
+<body class="hold-transition login-page">
+<div class="login-box">
+    <div class="login-logo">
+        <a href="../../index2.html"><b>Health</b>Pro</a>
+    </div>
+
+    <div class="card">
+        <div class="card-body login-card-body">
+
+            <form action="/dashboard" method="GET">
+                <div class="input-group mb-3">
+                    <input type="email" class="form-control" placeholder="Email">
+                    <div class="input-group-append">
+                        <div class="input-group-text">
+                            <span class="fas fa-envelope"></span>
+                        </div>
+                    </div>
+                </div>
+                <div class="input-group mb-3">
+                    <input type="password" class="form-control" placeholder="Password">
+                    <div class="input-group-append">
+                        <div class="input-group-text">
+                            <span class="fas fa-lock"></span>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+
+
+                    <div class="col-4">
+                        <button type="submit" class="btn btn-primary btn-block">Sign In</button>
+                    </div>
+
+                </div>
+            </form>
+
+
+        </div>
+
+    </div>
+</div>
+
+
+<script src="../../plugins/jquery/jquery.min.js"></script>
+
+<script src="../../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+<script src="../../dist/js/adminlte.min.js?v=3.2.0"></script>
+</body>
+</html>
--- resources/views/template/navbar.blade.php
+++ resources/views/template/navbar.blade.php
@@ -7,7 +7,8 @@
     <ul class="navbar-nav ml-auto">
 
         <li class="nav-item">
-            <a class="nav-link" data-widget="control-sidebar" data-slide="true" href="#"><i
+
+            <a class="nav-link"  href="/">  Logout &nbsp; <i
                     class="fas fa-th-large"></i></a>
         </li>
     </ul>
